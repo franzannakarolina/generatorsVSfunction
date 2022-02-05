c7aa92b91b8a414ec7ea770c4b00f4a510dd0ad4
franzannakarolina <nerdstecnologia0@gmail.com>
2022-02-04 22:44:39 -0300

    estudando generators e functions JS

---
+++ preclass/server/server1.mjs
@@ -0,0 +1,23 @@
+import { createServer } from 'http'
+import { randomUUID } from 'crypto'
+import { parse } from 'url'
+const PORT = 3000;
+async function handler(request, response) {
+    if (
+        request.method === 'GET' &&
+        request.url.includes('products')
+    ) {
+
+        const { query: { productName } } = parse(request.url, true)
+
+        return response.end(JSON.stringify({
+            id: randomUUID(),
+            product: productName,
+        }))
+    }
+
+    return response.end('hey!')
+}
+
+createServer(handler)
+    .listen(PORT, () => console.log(`products API is running at ${PORT}`))
